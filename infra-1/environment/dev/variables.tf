ba364d7de43859b3b399c88e87ac7e1fd70fc22c
Yagmur ULAS <yagmurulass@gmail.com>
2024-11-11 02:09:22 +0300

    Update variables.tf

--- infra-1/environment/dev/variables.tf
+++ infra-1/environment/dev/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
-  description = "The AWS region to deploy the EKS cluster."
+  description = "AWS region to deploy the EKS cluster."
   type        = string
   default     = "eu-central-1"
 }
 
 variable "cluster_name" {
-  description = "The name of the EKS cluster."
+  description = "name of the EKS cluster."
   type        = string
   default     = "dev-cluster"
 }
@@ -16,14 +16,51 @@ variable "cluster_version" {
   default     = "1.21"
 }
 
-variable "vpc_id" {
-  description = "The VPC ID where the EKS cluster will be deployed."
+variable "vpc_name" {
+  description = "New VPC name where the EKS cluster will be deployed."
   type        = string
+}
+
+variable "vpc_version" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string  
 }
 
 variable "subnets" {
   description = "The subnets IDs where the EKS cluster will be deployed."
   type        = list(string)
+}
+
+variable "azs" {
+  description = "The availability zones"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "The private subnets"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "The public subnets"
+  type        = list(string)
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway"
+  type        = bool
+}
+
+variable "single_nat_gateway" {
+  description = "Single NAT Gateway"
+  type        = bool
+}
+
+variable "node_group_name" {
+  type = string
 }
 
 variable "desired_capacity" {
